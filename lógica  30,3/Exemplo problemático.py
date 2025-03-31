e786e8f0f3df9a698f81335936781c7cb2a86729
Kelvin Gabriel <kelvingabrielmorreno2000@gmail.com>
2025-03-31 13:26:08 -0300

    Cria exercicio decremento while

---
+++ lógica  30,3/Exemplo problemático.py
@@ -0,0 +1,5 @@
+x = 5
+while x > 0:
+    print('Loop infinito!')
+    x = x -1
+    # Falta decrementar x
--- lógica  30,3/while.py
+++ lógica  30,3/while.py
@@ -1,1 +1,7 @@
 print("hello")
+print()
+
+contador = 1
+while contador <=5:
+    print(contador)
+    contador = contador + 1
